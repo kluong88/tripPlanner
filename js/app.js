c7c356558d6be3bd7ca76323793ab9cac8fae5d6
Kevin Luong <luongk88@gmail.com>
2020-05-22 11:52:03 -0500

    Delegated eventlistener for form submission. Origin search now fetches 10 results based on keyword search

--- js/app.js
+++ js/app.js
@@ -1,23 +1,42 @@
 const forms = document.querySelectorAll(`form`);
 const inputs = document.querySelectorAll(`input`);
+const mapBoxApiKey = `pk.eyJ1Ijoia2x1b25nODgiLCJhIjoiY2thNWlsejBzMDBpaTNkcWYwZ3VmbjF0ZCJ9.Dt-l5W_looV6UXMeDr905w`;
+const transitApiKey = `BbxBNg96RH903JTtJFdZ`;
+const boundBox = `-97.325875,49.766204,-96.953987,49.99275`
 
 
 
 for (const form of forms) {
-
   form.onsubmit = event => {
-
     if (event.target.className === `origin-form`) {
       console.log(`origin input:`);
       console.log(inputs[0].value);
+      searchOrigin(inputs[0].value);
       inputs[0].value = ``;
     } else {
       console.log(`destination input:`)
       console.log(inputs[1].value);
+      searchDestination(inputs[1].value);
+
       inputs[1].value = ``;
     }
     event.preventDefault();
   }
 }
 
-// https: //api.mapbox.com/geocoding/v5/mapbox.places/mall.json?access_token=pk.eyJ1Ijoiam9obmF0aGFubml6aW9sIiwiYSI6ImNqcG5oZjR0cDAzMnEzeHBrZGUyYmF2aGcifQ.7vAuGZ0z6CY0kXYDkcaOBg&limit=10&bbox=-97.325875,49.766204,-96.953987,49.99275
+
+function searchOrigin(searchValue) {
+  fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchValue}.json?access_token=${mapBoxApiKey}&limit=10&bbox=${boundBox}`)
+    .then(resp => resp.json())
+    .then(originSearch => {
+      console.log(originSearch);
+    })
+
+};
+
+function searchDestination(searchValue) {
+  console.log(`You searched for destination: ${searchValue}`);
+
+
+
+};
