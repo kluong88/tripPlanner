9294dedf0ed11fcb4d3a9bbcd3292516d6a0a7fb
Kevin Luong <luongk88@gmail.com>
2020-05-22 13:02:20 -0500

    Removed empty fields from index.html. Search now populates list of 10 search results for origin and destination

--- js/app.js
+++ js/app.js
@@ -2,21 +2,24 @@ const forms = document.querySelectorAll(`form`);
 const inputs = document.querySelectorAll(`input`);
 const mapBoxApiKey = `pk.eyJ1Ijoia2x1b25nODgiLCJhIjoiY2thNWlsejBzMDBpaTNkcWYwZ3VmbjF0ZCJ9.Dt-l5W_looV6UXMeDr905w`;
 const transitApiKey = `BbxBNg96RH903JTtJFdZ`;
-const boundBox = `-97.325875,49.766204,-96.953987,49.99275`
+const boundBox = `-97.325875,49.766204,-96.953987,49.99275`;
+const originsList = document.querySelector(`.origins`);
+const destinationsList = document.querySelector(`.destinations`);
+const originContainer = document.querySelector(`.origin-container`);
+const listEle = document.querySelectorAll(`li`);
+const selected = document.querySelector(`li.selected`);
 
 
 
 for (const form of forms) {
   form.onsubmit = event => {
     if (event.target.className === `origin-form`) {
-      console.log(`origin input:`);
-      console.log(inputs[0].value);
       searchOrigin(inputs[0].value);
+      originsList.innerHTML = ``
       inputs[0].value = ``;
     } else {
-      console.log(`destination input:`)
-      console.log(inputs[1].value);
       searchDestination(inputs[1].value);
+      destinationsList.innerHTML = ``
 
       inputs[1].value = ``;
     }
@@ -24,18 +27,61 @@ for (const form of forms) {
   }
 }
 
+originContainer.onclick = event => {
+
+  if (event.target.closest(`LI`)) {
+    listEle.forEach(element => {
+      element.className = ``;
+    })
+    console.log(`hi`);
+    event.target.closest(`LI`).classList.add(`selected`);
+  }
+
+}
+
 
 function searchOrigin(searchValue) {
   fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchValue}.json?access_token=${mapBoxApiKey}&limit=10&bbox=${boundBox}`)
     .then(resp => resp.json())
     .then(originSearch => {
-      console.log(originSearch);
+
+      originSearch.features.forEach(result => {
+        updateOriginSearch(result);
+      })
     })
 
 };
 
+function updateOriginSearch(searchResults) {
+
+  originsList.insertAdjacentHTML(`beforeend`, `
+  <li data-long="-97.19167" data-lat="49.815176" class="">
+  <div class="name">${searchResults.text}</div>
+  <div>${searchResults.properties.address}</div>
+</li>  
+  `)
+}
+
+function updateDestinationSearch(searchResults) {
+  destinationsList.insertAdjacentHTML(`beforeend`, `
+  <li data-long="-97.19167" data-lat="49.815176" class="">
+  <div class="name">${searchResults.text}</div>
+  <div>${searchResults.properties.address}</div>
+</li>  
+  `)
+
+
+}
+
 function searchDestination(searchValue) {
-  console.log(`You searched for destination: ${searchValue}`);
+  fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchValue}.json?access_token=${mapBoxApiKey}&limit=10&bbox=${boundBox}`)
+    .then(resp => resp.json())
+    .then(destinationSearch => {
+
+      destinationSearch.features.forEach(result => {
+        updateDestinationSearch(result);
+      })
+    })
 
 
 
